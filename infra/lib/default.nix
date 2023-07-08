21407718686bc1248853275bab1f7430239f9c9b
Tristan Schrader <tristanschrader@proton.me>
2023-07-08 14:55:43 -0700

    fix toYAML function to define a list of one item

--- infra/lib/default.nix
+++ infra/lib/default.nix
@@ -5,7 +5,7 @@
       input_f = builtins.toFile "obj.json" (builtins.toJSON obj);
       command = "remarshal -if json -i \"${input_f}\" -of yaml -o \"$out\"";
       output_f = pkgs.runCommand "to-yaml" { nativeBuildInputs = [ pkgs.remarshal ]; } command;
-      values = builtins.readFile output_f;
+      values = [ (builtins.readFile output_f) ];
     in values;
   subTemplateCmds = { template, cmds ? {} }:
     let
