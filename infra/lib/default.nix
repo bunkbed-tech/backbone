815a5065dc2058e152f4aad6c906d938038a1838
Tristan Schrader <tristan@climaxfoods.com>
2023-07-09 23:22:14 -0700

    add function to make resources with same function
    you pass a mkResource function to build a resource block from a name and
    an attrset, as well as an optional getName function to grab a new name
    from the resultant resource block. the default is to treat it like a
    kubernetes config, so to use `resource: resource.metadata.name` as the
    name acessor

--- infra/lib/default.nix
+++ infra/lib/default.nix
@@ -1,16 +1,21 @@
 { pkgs }:
 {
   toYAML = obj:
-    let
-      input_f = builtins.toFile "obj.json" (builtins.toJSON obj);
-      command = "remarshal -if json -i \"${input_f}\" -of yaml -o \"$out\"";
-      output_f = pkgs.runCommand "to-yaml" { nativeBuildInputs = [ pkgs.remarshal ]; } command;
-      values = [ (builtins.readFile output_f) ];
-    in values;
+  let
+    input_f = builtins.toFile "obj.json" (builtins.toJSON obj);
+    command = "remarshal -if json -i \"${input_f}\" -of yaml -o \"$out\"";
+    output_f = pkgs.runCommand "to-yaml" { nativeBuildInputs = [ pkgs.remarshal ]; } command;
+    values = [ (builtins.readFile output_f) ];
+  in values;
   subTemplateCmds = { template, cmds ? {} }:
-    let
-      contents_old = builtins.readFile template;
-      cmds_sub_fmt = map (cmd: "\\${cmd}") (builtins.attrNames cmds);
-      contents_new = builtins.replaceStrings cmds_sub_fmt (builtins.attrValues cmds) contents_old;
-    in contents_new;
+  let
+    contents_old = builtins.readFile template;
+    cmds_sub_fmt = map (cmd: "\\${cmd}") (builtins.attrNames cmds);
+    contents_new = builtins.replaceStrings cmds_sub_fmt (builtins.attrValues cmds) contents_old;
+  in contents_new;
+  mkResources = { getName ? (resource: resource.metadata.name) }: mkResource:
+    pkgs.lib.attrsets.mapAttrs' (_name: obj:
+    let resource = mkResource _name obj;
+    in pkgs.lib.attrsets.nameValuePair (getName resource) resource
+    );
 }
