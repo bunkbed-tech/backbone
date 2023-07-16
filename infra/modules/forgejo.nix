1a90cb12f65f0d8655c9e13db8251df7e95497c7
Tristan Schrader <tristan@climaxfoods.com>
2023-07-16 15:08:23 -0700

    finally deployed forgejo correctly
    we had to manage a PV and PVC for the Forgejo module (and Postgres
    database) by ourselves because we are using a Local Storage class (i.e.
    a path mounted on the server node), which does not support Dynamic
    Provisioning. to achieve this, we created resources for each, allowing
    the volume to attach to the correct node with the
    `kubernetes.io/hostname` label.
    
    while this solved the persistence issue with the application itself, the
    Postgres dependency for some reason, did not respond to the PVC
    configuration and we filed a [bug report](https://codeberg.org/forgejo-contrib/forgejo-helm/issues/162) about this in the Forgejo Helm
    chart repository. Ultimately, we decided to manage the database
    separately using configuration options that did ultimately work.

--- infra/modules/forgejo.nix
+++ infra/modules/forgejo.nix
@@ -2,13 +2,76 @@
 let
   name = "forgejo";
   namespace = name;
+  user = name;
+  database = name;
+  postgres = "${name}-postgres";
+  storage = "18Gi";
 in {
-  resource.helm_release.forgejo = {
+  resource.kubernetes_persistent_volume.forgejo = {
     depends_on = [ "kubernetes_storage_class.default" ];
+    metadata.name = name;
+    spec.storage_class_name = config.resource.kubernetes_storage_class.default.metadata.name;
+    spec.capacity.storage = storage;
+    spec.access_modes = [ "ReadWriteOnce" ];
+    spec.persistent_volume_source.host_path.path = "/k8s/${name}";
+    spec.node_affinity.required.node_selector_term = [
+      {
+        match_expressions = {
+          key = "kubernetes.io/hostname";
+          operator = "In";
+          values = [ "sirver" ];
+        };
+      }
+    ];
+  };
+  resource.kubernetes_persistent_volume_claim.forgejo = {
+    depends_on = [ "kubernetes_persistent_volume.forgejo" "kubernetes_namespace.forgejo" ];
+    metadata = { inherit name namespace; };
+    spec.access_modes = [ "ReadWriteOnce" ];
+    spec.resources.requests.storage = storage;
+    spec.volume_name = config.resource.kubernetes_persistent_volume.forgejo.metadata.name;
+  };
+
+  resource.kubernetes_namespace.forgejo = { metadata.name = namespace; };
+  resource.random_password.forgejo-postgres = { length = 24; };
+  resource.kubernetes_secret.forgejo-postgres = {
+    depends_on = [ "kubernetes_namespace.forgejo" ];
+    metadata.name = postgres;
+    metadata.namespace = namespace;
+    binary_data.password = "\${ base64encode(random_password.forgejo-postgres.result) }";
+  };
+  resource.helm_release.forgejo = {
+    depends_on = [ "kubernetes_persistent_volume_claim.forgejo" ];
     inherit name namespace;
-    create_namespace = true;
     repository = "oci://codeberg.org/forgejo-contrib";
     chart = "forgejo";
     version = "0.8.6";
+    values = pkgs.lib.backbone.toYAML {
+      persistence.existingClaim = name;
+      postgresql.enabled = false;
+      gitea.config.database = {
+        DB_TYPE = "postgres";
+        HOST = "${postgres}-postgresql.${namespace}.svc.cluster.local";
+        NAME = database;
+        USER = user;
+        PASSWD = "\${ random_password.forgejo-postgres.result }";
+      };
+    };
+  };
+  resource.helm_release.forgejo-postgres = {
+    depends_on = [ "kubernetes_persistent_volume_claim.forgejo" "kubernetes_secret.forgejo-postgres" ];
+    name = postgres;
+    namespace = namespace;
+    repository = "oci://registry-1.docker.io/bitnamicharts";
+    chart = "postgresql";
+    version = "12.6.6";
+    values = pkgs.lib.backbone.toYAML {
+      auth.enablePostgresUser = false;
+      auth.username = user;
+      auth.database = database;
+      auth.existingSecret = postgres;
+      primary.persistence.existingClaim = name;
+      volumePermissions.enabled = true;
+    };
   };
 }
