f269eb1607af1746f04d18aea8a2b16ffaaadf69
Tristan Schrader <tristan@climaxfoods.com>
2023-07-22 21:19:08 -0700

    deploy traefik as a helm release
    included arguments recommended by the tutorial, but not sure what they
    do (--api.insecure and --accesslog)

--- infra/modules/traefik.nix
+++ infra/modules/traefik.nix
@@ -3,63 +3,25 @@ let
   name = "traefik";
   namespace = name;
   labels.app = name;
-  services = { dashboard.port = 8080; web.port = 80; };
+  services.web.port = 80;
+  mkPort = name: type: { name = name; ${type} = services.${name}.port; };
 in {
-  config = lib.mkIf (config.kubernetes.version == "k8s") {
-    resource.kubernetes_namespace.traefik = { metadata.name = name; };
-    resource.kubernetes_cluster_role.traefik = {
-      metadata.name = name;
-      rule = [
+  resource.helm_release.traefik = {
+    inherit name namespace;
+    repository = "https://traefik.github.io/charts";
+    chart = "traefik";
+    version = "23.1.0";
+    create_namespace = true;
+    values = pkgs.lib.backbone.toYAML {
+      additionalArguments = [
+        "--api.insecure"
+        "--accesslog"
+      ];
+    };
+  };
         {
-          api_groups = [ "" ];
-          resources = [ "services" "endpoints" "secrets" ];
-          verbs = [ "get" "list" "watch" ];
-        }
-        {
-          api_groups = [ "extensions" "networking.k8s.io" ];
-          resources = [ "ingresses" "ingressclasses" ];
-          verbs = [ "get" "list" "watch" ];
-        }
-        {
-          api_groups = [ "extensions" "networking.k8s.io" ];
-          resources = [ "ingresses/status" ];
-          verbs = [ "update" ];
         }
       ];
     };
-    resource.kubernetes_service_account.traefik = {
-      depends_on = [ "kubernetes_namespace.traefik" ];
-      metadata = { inherit name namespace; };
-    };
-    resource.kubernetes_cluster_role_binding.traefik = {
-      depends_on = [ "kubernetes_cluster_role.traefik" "kubernetes_service_account.traefik" ];
-      metadata.name = name;
-      role_ref = { api_group = "rbac.authorization.k8s.io"; kind = "ClusterRole"; name = name; };
-      subject = [ { inherit name namespace; kind = "ServiceAccount"; } ];
-    };
-    resource.kubernetes_deployment.traefik = {
-      depends_on = [ "kubernetes_cluster_role_binding.traefik" ];
-      metadata = { inherit name namespace labels; };
-      spec.replicas = 1;
-      spec.selector.match_labels = labels;
-      spec.template.metadata.labels = labels;
-      spec.template.spec.service_account_name = name;
-      spec.template.spec.container = [
-        {
-          name = name;
-          image = "traefik:v2.10";
-          args = [ "--api.insecure" "--providers.kubernetesingress" ];
-          port = lib.attrsets.mapAttrsToList (_name: service: { name = _name; container_port = service.port; }) services;
-        }
-      ];
-    };
-    resource.kubernetes_service = pkgs.lib.backbone.mkResources {} (_name: service: {
-      depends_on = [ "kubernetes_deployment.traefik" ];
-      metadata.name = "${name}-${_name}";
-      metadata.namespace = namespace;
-      spec.type = "LoadBalancer";
-      spec.port = [ { port = service.port; target_port = _name; } ];
-      spec.selector = labels;
-    }) services;
   };
 }
