c48aed6a87cc6af1bd3078629f18542f67c6440e
Tristan Schrader <tristan@climaxfoods.com>
2023-07-22 21:21:27 -0700

    deploy test "whoami" app for Traefik configuration
    the IngressRoute CRD allows us to say here that whoami.bunkbed.tech will
    have a TLS certificate from Let's Encrypt and will go to this deployment
    through our secure web entrypoint

--- infra/modules/traefik.nix
+++ infra/modules/traefik.nix
@@ -25,9 +25,36 @@ in {
       };
     };
   };
+  resource.kubernetes_manifest.whoami = {
+    manifest = {
+      kind = "IngressRoute";
+      apiVersion = "traefik.io/v1alpha1";
+      metadata.name = "whoami-tls";
+      metadata.namespace = "default";
+      spec.entryPoints = [ "websecure" ];
+      spec.routes = [
         {
+          match = "Host(`whoami.bunkbed.tech`)";
+          kind = "Rule";
+          services = [ { name = "whoami"; port = services.web.port; } ];
         }
       ];
+      spec.tls.certResolver = "letsencrypt";
     };
+  };
+  resource.kubernetes_deployment.whoami = {
+    metadata.name = "whoami";
+    metadata.labels.app = "whoami";
+    spec.replicas = 1;
+    spec.selector.match_labels.app = "whoami";
+    spec.template.metadata.labels.app = "whoami";
+    spec.template.spec.container = [
+      { name = "whoami"; image = "traefik/whoami"; port = [ (mkPort "web" "container_port") ]; }
+    ];
+  };
+  resource.kubernetes_service.whoami = {
+    metadata.name = "whoami";
+    spec.selector.app = "whoami";
+    spec.port = [ (mkPort "web" "port") ];
   };
 }
