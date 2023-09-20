4add69201c0df6dd68cf60344174a38ff3ee7d7e
Tristan Schrader <tristan@climaxfoods.com>
2023-09-19 23:09:33 -0700

    add k3d terranix module to create cluster
    this terraform configuration is the same as running:
    
    ```sh
    k3d cluster create bunkbed \
    --k3s-arg "--disable=traefik@server:0" \
    --port "4443:443@loadbalancer" \
    --agents 3
    ```
    
    you have to explicitly update the kube/config file with this.
    we make the traefik helm release only dependent on the cluster resource
    because it is the target of the first stage of the entire setup. because
    the other services are dependent on traefik, we just need traefik to
    require the cluster

---
+++ src/k3d.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+{
+  config.terraform.required_providers.k3d = { source = "pvotal-tech/k3d"; version = "0.0.7"; };
+  config.provider.k3d = {};
+  config.resource.k3d_cluster.bunkbed = {
+    name = "bunkbed";
+    agents = 3;
+    port = { host_port = 4443; container_port = 443; node_filters = [ "loadbalancer" ]; };
+    k3s = { extra_args = [ { arg = "--disable=traefik"; node_filters = [ "server:0" ]; } ]; };
+    kubeconfig.update_default_kubeconfig = true;
+  };
+  config.resource.helm_release.traefik.depends_on = [ "k3d_cluster.bunkbed" ];
+}
