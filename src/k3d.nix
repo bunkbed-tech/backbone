a6210d1c1a4804d477f0124b14b3b3dcc9cb4dd6
Tristan Schrader <tristan@climaxfoods.com>
2023-09-28 19:30:38 -0700

    pin k3d image to working version

--- src/k3d.nix
+++ src/k3d.nix
@@ -4,6 +4,7 @@
   config.provider.k3d = {};
   config.resource.k3d_cluster.bunkbed = {
     name = "bunkbed";
+    image = "docker.io/rancher/k3s:v1.27.5-k3s1";
     agents = 3;
     port = { host_port = 4443; container_port = 443; node_filters = [ "loadbalancer" ]; };
     k3s = { extra_args = [ { arg = "--disable=traefik"; node_filters = [ "server:0" ]; } ]; };
